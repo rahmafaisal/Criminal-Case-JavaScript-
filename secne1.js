852ffd150ec7656bbe681a05be3a3af31872fcf9
rahmafaisal <45911037+rahmafaisal@users.noreply.github.com>
2018-12-28 20:21:57 +0200

    scene pages
    this is the first prototype for the scene page 
    html and css 
    JavaScript still working on

---
+++ secne1.js
@@ -0,0 +1,14 @@
+
+function effectonimg()
+{
+    this.style.display='none';
+    var item=document.getElementById('items');
+    
+
+
+}
+imag1.addEventListener('click',effectonimg);
+imag2.addEventListener('click',effectonimg);
+imag3.addEventListener('click',effectonimg);
+imag4.addEventListener('click',effectonimg);
+imag5.addEventListener('click',effectonimg);
